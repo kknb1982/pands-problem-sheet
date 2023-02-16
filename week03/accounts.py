657ebef3bc71775d2def47b3109e1588db641d88
Kirstin Barnett <G00425638@atu.ie>
2023-02-16 11:32:49 +0000

    week03

--- week03/accounts.py
+++ week03/accounts.py
@@ -1,7 +1,11 @@
 # A program that reads in an account number and only shows the last 4 digits the rest replaced by x
 # Author: Kirstin Barnett
 
-bank_account_number = input("Please enter a 10 digit account number: ")
+bank_account_number = input("Please enter an account number: ")
 last_4_digits = bank_account_number[-4:]
 
-print (f"xxxxxx{last_4_digits}")
+length_account_number = len(bank_account_number)
+number_of_xs = length_account_number - 4
+masked_digits = number_of_xs * 'x'
+
+print (f"{masked_digits}{last_4_digits}")
