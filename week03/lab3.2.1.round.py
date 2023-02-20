add6ead66431ffde439a0c95db68a9eb4283a11a
Kirstin Barnett <123597290+kknb1982@users.noreply.github.com>
2023-02-20 18:16:20 +0000

    Update and rename lab3.2.round.py to lab3.2.1.round.py

--- week03/lab3.2.1.round.py
+++ week03/lab3.2.1.round.py
@@ -1,5 +1,6 @@
 # Input a float and output it rounded to an integer
 # Author = Kirstin Barnett
+# Uses code from Andrew Beatty
 
 number_to_round = float(input ("Enter a float number: "))
 rounded_number = round (number_to_round)
