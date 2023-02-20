d84369bc1fc3f3fe55518946789cc9b2da167691
Kirstin Barnett <123597290+kknb1982@users.noreply.github.com>
2023-02-20 17:58:31 +0000

    Update and rename lab3.1.sub.py to lab3.1.2.sub.py

--- week03/lab3.1.2.sub.py
+++ week03/lab3.1.2.sub.py
@@ -1,5 +1,6 @@
 # Program to subtract one number from another
 # Author: Kirstin Barnett
+# Based on code by Andrew Beatty
 
 x = int(input("Enter first number: "))
 y = int(input("Enter second number: "))
