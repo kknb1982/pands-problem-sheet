4b71428fe2155f13002966b400451a5e0d1c7605
Kirstin Barnett <123597290+kknb1982@users.noreply.github.com>
2023-02-20 18:05:56 +0000

    Update and rename lab3.1.div.py to lab3.1.3.div.py

--- week03/lab3.1.3.div.py
+++ week03/lab3.1.3.div.py
@@ -1,5 +1,6 @@
 # Divides one number by another and gives the integer and remainder
 # Author = Kirstin Barnett
+# Based on code from Andrew Beatty
 
 x = int(input("Enter the first number: "))
 y = int(input("Enter the number you want to divide by: "))
