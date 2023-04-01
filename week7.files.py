0ab4e591a05081a6b6629b814e2c30c5bb382afc
Kirstin Barnett <G00425638@atu.ie>
2023-04-01 11:45:20 +0100

    Update to week7.files.py to use import sys

--- week7.files.py
+++ week7.files.py
@@ -1,8 +1,10 @@
 # Program to open a file and count the number of occurrences of the character e within it
 # Author: Kirstin Barnett
 
+import sys
+
 character = "e"
-FILENAME = input(f"What file would you like to count occurences of '{character}' in?: ")
+FILENAME = sys.argv[1]
 
 def number():
     with open(FILENAME) as f:
@@ -11,6 +13,17 @@ def number():
         occurences = lower_case.count(character)
         return occurences
 
-number_of_es = number()
-print (f"In the file {FILENAME} there are {number_of_es} occurences of the character '{character}'.")
+try:
+    with open(FILENAME) as f:
+        data = f.read()
+
+except FileNotFoundError:
+     print ("Check the spelling of the file name. \nIf the file is not in the same folder as the sys.py program please enter the full address of the file.")
+
+except Exception as e:
+    print (f"Something has gone wrong. Here is the error information: \n{e}")
+
+else:
+    number_of_es = number()
+    print (f"In the file {FILENAME} there are {number_of_es} occurences of the character '{character}'.")g
 
