988fc18f0817774dd20f0f29fd01362eea80ff86
Kirstin Barnett <G00425638@atu.ie>
2023-04-02 12:45:21 +0100

    update to simplify code to use if and elif

--- week4.collatz.py
+++ week4.collatz.py
@@ -1,23 +1,27 @@
 # Coding the collatz conjecture
+# A positive integer is inputted
+# If the number is even this is divided by 2
+# If the number is odd it is multiplied by 3 and 1 added
+# This cycle continues until 1 is reached
 
 # Author: Kirstin Barnett
 
+# Opens a list to store the solutions
 numbers = []
 
-def collatz(number):
-    if number  % 2 == 0:
-        return number //2
-    elif number % 2 != 0:
-        return number *3 + 1
-    else:
-        print ('Something went wrong')
+# Asks for a positive integer to be input
+number = int(input ("Please enter a positive integer: "))
 
-number = int(input ("Please enter a positive integer?: "))
-
+# While number is not 1 this code is run to divide even numbers by 2 and odd numbers are multiplied by 3 plus 1. 
+# The outputs are added to the numbers list and stored as integers
 while number != 1:
-    numbers.append(number)
-    number = collatz(number)
-else:
-    numbers.append(number) 
+    if number % 2 == 0:
+        number = number / 2
+        numbers.append(int(number))
+    
+    else:
+        number = number *3 +1
+        numbers.append(int(number))
 
+# When 1 is reached this is printed
 print(*numbers)
