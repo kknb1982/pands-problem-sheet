b01d0481e8aa7641e2b57483e8ddafcc5d6e2d4b
Kirstin Barnett <G00425638@atu.ie>
2023-04-02 12:17:24 +0100

    Added comments to the code for  week 3

--- week3.accounts.py
+++ week3.accounts.py
@@ -4,11 +4,13 @@
 
 bank_account_number = input("Please enter an account number: ")
 
-# Strips the last 4 digits from the bank account numbergit pusll
+# Strips the last 4 digits from the bank account number
 last_4_digits = bank_account_number[-4:]
 
+# Calculates the length of the bank account number and therefore the number of xs to be used in the masking
 length_account_number = len(bank_account_number)
 number_of_xs = length_account_number - 4
 masked_digits = number_of_xs * 'x'
 
+# Prints out the masked account number
 print (f"{masked_digits}{last_4_digits}")
