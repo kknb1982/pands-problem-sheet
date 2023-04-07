bdd6717904f4c7593d77f22fe54730f2290e1321
Kirstin Barnett <G00425638@atu.ie>
2023-04-07 12:06:55 +0100

    Added comments to code

--- week5.weekday.py
+++ week5.weekday.py
@@ -1,10 +1,12 @@
 # Outputs whether today is a weekday or not and prints out appropriate text
 # Author: Kirstin Barnett
 
+# To work out the day of the week as a number
 from datetime import date
 today = date.today()
 day = today.weekday()
 
+# Saturday and Sunday are 5 and 6
 if day > 4:
     print ("It is the weekend, yay!")
 
