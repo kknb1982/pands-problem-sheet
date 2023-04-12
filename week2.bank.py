b780aa5b6ddff12275d38fec03de26021a82dd7a
Kirstin Barnett <G00425638@atu.ie>
2023-04-12 09:55:59 +0100

    updated the final part to use slicing to format

--- week2.bank.py
+++ week2.bank.py
@@ -6,8 +6,9 @@ amount1 = int(input("Enter the first amount in cents: "))
 amount2 = int(input ("Enter the second amount in cents: "))
 
 #Sums the amount
-sum = amount1 + amount2
+sum = str(amount1 + amount2)
 
 # Converts the sum in euros and cents and outputs in €xxx.xxx
-total = int(sum)/100
-print (f"The sum of these is €{total}")
+cents = sum[-2:]
+euros = sum[:(len(sum)-2)]
+print (f"The sum of these is €{euros}.{cents}")
